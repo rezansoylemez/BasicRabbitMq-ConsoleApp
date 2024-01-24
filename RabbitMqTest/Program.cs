e8718b3c70a6addfdf472eceaff32767b26f1c26
RezanSoylemez <rezan.sm@gmail.com>
2024-01-24 17:18:23 +0300

    Add project files.

---
+++ RabbitMqTest/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Text.Json.Serialization;
+
+Console.WriteLine("Hello, World!");
+
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("test-queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = new
+{
+    Name = "Producer",
+    Message = "Hello!"
+};
+
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+
+channel.BasicPublish("","test-queue",null, body);

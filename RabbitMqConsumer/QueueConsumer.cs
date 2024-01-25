0284d8f67ab65860aaf666c9acfdfaed81ed9b64
RezanSoylemez <rezan.sm@gmail.com>
2024-01-25 10:17:12 +0300

    Featured - ConsumerAndProducerClassesAreAdded

---
+++ RabbitMqConsumer/QueueConsumer.cs
@@ -0,0 +1,31 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace RabbitMqConsumer;
+
+public static class QueueConsumer
+{
+    public static void Consume(IModel channel)
+    {
+        channel.QueueDeclare("test-queue",
+        durable: true,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (sender, e) =>
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine(message);
+        };
+
+        channel.BasicConsume("test-queue", true, consumer);
+
+        Console.WriteLine("Consumer Started");
+        Console.ReadLine();
+    }
+}
